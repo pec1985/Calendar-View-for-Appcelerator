6b8cd8268120b9092d7204238ee26da555b77bbc
pec1985 <pedro.tma@gmail.com>
2010-08-31 19:42:21 -0400

    beta 5

--- calendar.js
+++ calendar.js
@@ -26,25 +26,34 @@ var label = Ti.UI.createButton({top:0,height:50,width:200,title:'Go Back'});
 // b = month
 // c = day
 var showCal = function(a,b,c){
+	/*
+	Ti.API.info('');
+	Ti.API.info('a: '+a);
+	Ti.API.info('b: '+b);
+	Ti.API.info('c: '+c);
+	Ti.API.info('');
+	*/
 	// main two views
 	barView = Ti.UI.createView({width:320,height:45,top:0,left:0,backgroundImage:'barview.png'});
 	calendarView = Ti.UI.createView({width:'auto',left:3,height:'auto',layout:'horizontal'});
 	
 	//set up the date according to the values a, b, and c
 	var date = new Date(a,b,c);	
-	var year = date.getFullYear();
-	var month = date.getMonth();
-	var daysInMonth = 32 - new Date(year,month,32).getDate();
+	var year = a; //date.getFullYear();
+	var month = b; //date.getMonth();
+	var daysInMonth = 32 - new Date(a,b,32).getDate();
 	var dayOfMonth = date.getDate();
 	var dayOfWeek = new Date(year,month,1).getDay();
 
 	var selectedDay = dayOfMonth;
 
 	//some things to show up on the console
+/*
 	Ti.API.info('year: '+year);
-	Ti.API.info('month :'+month);
-	Ti.API.info('day of the month :'+dayOfMonth);
-	Ti.API.info('first day of the week' +dayOfWeek);
+	Ti.API.info('month: '+month);
+	Ti.API.info('day of the month: '+dayOfMonth);
+	Ti.API.info('first day of the week: ' +dayOfWeek);
+*/
 	
 	//what month are we on?
 	var thisMonth = null;
@@ -84,16 +93,6 @@ var showCal = function(a,b,c){
 	barView.add(friLabel);
 	barView.add(satLabel);
 	
-	//set up the first day of the month on the calendar
-	var startDay = null;
-	if (dayOfWeek == 0){startDay = -1;}		// sunday
-	if (dayOfWeek == 1){startDay = 44;}		// monday
-	if (dayOfWeek == 2){startDay = 89;}		// tuesday
-	if (dayOfWeek == 3){startDay = 134;}	// wednesday
-	if (dayOfWeek == 4){startDay = 179;}	// thursday
-	if (dayOfWeek == 5){startDay = 224;}	// friday
-	if (dayOfWeek == 6){startDay = 269;}	// saturday
-
 	//function that makes everything work
 	var clicker = function(e){
 		e.addEventListener('click',function(thisday){
@@ -104,18 +103,50 @@ var showCal = function(a,b,c){
 			selected=thisday.source;
 		});
 	};
+	
+	
+	var i = 0;
+	
+	//figure out how many days where in last month
+	var daysInLastMonth = 32 - new Date(year,month-1,32).getDate();
+	
+	//figure out where to start counting the days from last month
+	startCountingLastMonthsDays = daysInLastMonth-dayOfWeek;
+	
+	//draw the last few days from last month to fill the gap
+	while (i < dayOfWeek){
+		var dayBefore =  Ti.UI.createButton({font:{fontWeight:'bold',fontSize:22},width:46, height:45,top:-1,backgroundSelectedImage:'button2.png'});
+		dayBefore.title=startCountingLastMonthsDays+1;
+		dayBefore.left=-1;
+		dayBefore.backgroundImage='button1.png';
+		dayBefore.color='#858A92';
+		calendarView.add(dayBefore);
+		startCountingLastMonthsDays++;
+	i++;
+	}
+	
+/*
+	This is not needed anymore, since the days missing from last month are drawn before
+
+	if (dayOfWeek == 0){startDay = -1;}		// sunday
+	if (dayOfWeek == 1){startDay = 44;}		// monday
+	if (dayOfWeek == 2){startDay = 89;}		// tuesday
+	if (dayOfWeek == 3){startDay = 134;}	// wednesday
+	if (dayOfWeek == 4){startDay = 179;}	// thursday
+	if (dayOfWeek == 5){startDay = 224;}	// friday
+	if (dayOfWeek == 6){startDay = 269;}	// saturday
+*/
+
+
 
 	//draw the calendar. The number of days of the month is the number of times that the loop repeats
-	var i = 1;
+	i = 1;
 	while (i < daysInMonth+1){
 		var day =  Ti.UI.createButton({title:i,font:{fontWeight:'bold',fontSize:22},width:46, height:45,top:-1,backgroundSelectedImage:'button2.png'});
 
 		//where to put the first day. (monday, tuesday, etc)
-		if (day.title == 1){
-			day.left=startDay;
-		}else{
 			day.left=-1;
-		}
+
 
 		//determine which day has to be special, this highlights 'today'
 		if (day.title == dayOfMonth){
@@ -135,13 +166,45 @@ var showCal = function(a,b,c){
 		calendarView.add(day);
 		i++;
 	}
+	
+	//this is just a new var, nothing special
+	var t = 0;
+	
+	//figure out how many days if next month need to be drawn to fill the gap
+	var startCountingNextMonthsDays = null;
+	while (t<7){
+		if(dayOfWeek==t && daysInMonth==31){startCountingNextMonthsDays = 4-t;}
+		if(dayOfWeek==t && daysInMonth==30){startCountingNextMonthsDays = 5-t;}
+		if(dayOfWeek==t && daysInMonth==29){startCountingNextMonthsDays = 6-t;}
+		if(dayOfWeek==t && daysInMonth==28){startCountingNextMonthsDays = 7-t;}
+		t++;
+	}
+	//if we get a negative number, correct it
+	if(startCountingNextMonthsDays == -1){startCountingNextMonthsDays=6;}
+	if(startCountingNextMonthsDays == -2){startCountingNextMonthsDays=5;}
+	
+	//draw the missing days of next month to fill the gap
+	i = 1;
+	while (i < startCountingNextMonthsDays+1){
+		var dayAfter =  Ti.UI.createButton({font:{fontWeight:'bold',fontSize:22},width:46, height:45,top:-1,backgroundSelectedImage:'button2.png'});
+		dayAfter.title=i;
+		dayAfter.left=-1;
+		dayAfter.backgroundImage='button1.png';
+		dayAfter.color='#858A92';
+		calendarView.add(dayAfter);
+		startCountingLastMonthsDays++;
+	i++;
+	}
+	
+	//add all of this mess to the window :)
+	
 	win.add(barView);
 	win.add(calendarView);
 	otherView.add(label);
 	win.add(bgView);
 	win.add(otherView);
 
-	win.title = selectedDay;
+	win.title = setMonth+1;
 };
 //---end of the main function---
 
@@ -150,11 +213,18 @@ var setDate = new Date();
 setYear = setDate.getFullYear();
 setMonth = setDate.getMonth();
 setDay = setDate.getDate();
+//draw the calendar, this calls the main function 'showCal' with the default values
+
+//showCal(setYear,3,setDay);
+showCal(setYear,setMonth,setDay);
+
 
 //change to next month
 next.addEventListener('click',function(e){
 	//add one month
 	setMonth++;
+	if (setMonth > 11){setMonth = setMonth-12; setYear++;}
+	
 	//remove everything from the window
 	win.remove(barView);
 	win.remove(calendarView);
@@ -168,6 +238,8 @@ next.addEventListener('click',function(e){
 prev.addEventListener('click',function(e){
 	//subtract one month
 	setMonth--;
+	if (setMonth < 0){setMonth = setMonth+12; setYear--;}
+	
 	//remove everything from the window
 	win.remove(barView);
 	win.remove(calendarView);
@@ -177,8 +249,6 @@ prev.addEventListener('click',function(e){
 	showCal(setYear,setMonth,setDay);
 });
 
-//draw the calendar, this calls the main function 'showCal' with the default values
-showCal(setYear,setMonth,setDay);
 
 //click on the main button to close the window and create a variable called 'finalDate' with the selected date to pass to the previous window
 label.addEventListener('click',function(){
